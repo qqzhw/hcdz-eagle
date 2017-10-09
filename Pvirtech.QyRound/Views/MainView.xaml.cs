78296602e6a18b85534359b4fe57f90c0cea4785
gufei <beyond@DESKTOP-GTH77VQ>
2017-10-09 15:31:29 +0800

    测试

--- Pvirtech.QyRound/Views/MainView.xaml.cs
+++ Pvirtech.QyRound/Views/MainView.xaml.cs
@@ -16,7 +16,7 @@ using System.Windows.Shapes;
 namespace Pvirtech.QyRound.Views
 {
 	/// <summary>
-	/// MainView.xaml 的交互逻辑  11
+	/// MainView.xaml 的交互逻辑
 	/// </summary>
 	public partial class MainView : UserControl
 	{
